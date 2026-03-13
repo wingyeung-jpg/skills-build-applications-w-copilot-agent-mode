4265796acc2d99f936704f318ecdff82fd4e93c1
wingyeung-jpg <wing.yeung@createfuture.com>
2026-03-13 09:52:58 +0000

    feat: add populate_db management command for test data

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(email='test@hero.com', name='Test Hero', team='marvel')
+        self.assertEqual(user.email, 'test@hero.com')
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        team = Team.objects.create(name='marvel', description='Marvel superheroes')
+        self.assertEqual(team.name, 'marvel')
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        user = User.objects.create(email='test@hero.com', name='Test Hero', team='marvel')
+        activity = Activity.objects.create(user=user, type='run', duration=30, date='2026-03-13')
+        self.assertEqual(activity.type, 'run')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        team = Team.objects.create(name='marvel', description='Marvel superheroes')
+        leaderboard = Leaderboard.objects.create(team=team, points=100)
+        self.assertEqual(leaderboard.points, 100)
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Pushups', description='Do pushups', difficulty='easy')
+        self.assertEqual(workout.name, 'Pushups')
